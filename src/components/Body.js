c1dcbac1bc88202bea073be950f0aacbc3e4ec17
Your Name <you@example.com>
2024-07-14 13:04:09 +0530

    Sign In/SIgn Up form & Header and Login Page

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "components/Login";
+import Browse from "components/Browse";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const Body = () => {
+  const appRouter = createBrowserRouter([
+    { path: "/", element: <Login /> },
+    { path: "/browse", element: <Browse /> },
+  ]);
+
+  return <RouterProvider router={appRouter} />;
+};
+
+export default Body;
